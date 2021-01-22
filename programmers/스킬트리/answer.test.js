b64730c36556381895be32562a8f59d28a297f28
dididy <dev.yongjaelee@gmail.com>
2021-01-22 23:32:21 +0900

    add: 프로그래머스/스킬트리

---
+++ programmers/스킬트리/answer.test.js
@@ -0,0 +1,30 @@
+function solution(skill, skill_trees) {
+  let answer = skill_trees.length;
+
+  skill_trees.forEach((val) => {
+    let check = skill.split('');
+    let flag = false;
+
+    for (let i = 0; i < val.length; i++) {
+      if (check.length && check.includes(val[i])) {
+        if (val[i] === check[0]) {
+          check.shift();
+        } else {
+          flag = true;
+          break;
+        }
+      }
+    }
+
+    if (flag) {
+      answer--;
+    }
+
+  });
+  return answer;
+}
+
+test('Test case', () => {
+  expect(solution('CBD', ['BACDE', 'CBADF', 'AECB', 'BDA'])).toBe(2);
+  expect(solution('C', ['A', 'ABC', 'C', 'CDS'])).toBe(4);
+});
